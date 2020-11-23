4981cc1fd34ea5003cb630fa2cfc0baa45a162fd
Nikita Novoselov <sarkomath@outlook.com>
2020-11-23 20:45:57 +1000

    first fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -3,9 +3,6 @@ public class BmiService {
         // рост в cм, вес в кг
         double calc_height = ((height * 0.01) * (height * 0.01)) ;
         double ibm = weight / calc_height;
-        if (ibm >= 25) {
-            System.out.println("Bad :(");
-            return ibm;
-        } else return ibm;
+        return ibm;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        double MassIndex = service.calculate(176,96 );
+        double MassIndex = service.calculate(176,70 );
         System.out.println("Your mass index is:");
         System.out.println(MassIndex);
+        if (MassIndex >= 25) {
+            System.out.println("Bad :(");
+        }
     }
 }
