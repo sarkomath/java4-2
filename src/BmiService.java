8f8f310ee0a4b037b43c4195537959729ff527a7
Nikita Novoselov <sarkomath@outlook.com>
2020-11-24 22:24:13 +1000

    second fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
     public double calculate(double height, int weight) {
         // рост в cм, вес в кг
-        double calc_height = ((height * 0.01) * (height * 0.01)) ;
-        double ibm = weight / calc_height;
+        double CalcHeight = ((height * 0.01) * (height * 0.01)) ;
+        double ibm = weight / CalcHeight;
         return ibm;
     }
 }
